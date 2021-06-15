4e75557e8edf2b7a3c65e2c02ba4d65942094d10
quentinDacquet <82867473+quentinDacquet@users.noreply.github.com>
2021-06-15 09:55:06 +0200

    Add files via upload

---
+++ competence 3 bootstarp/script/script.js
@@ -0,0 +1,75 @@
+function verif() {
+  let inputLogin = document.getElementById("login");
+  let login = document.getElementById("login").value;
+  let veriflogin = login.indexOf("@");
+  let acceptlogin = false;
+
+  if (veriflogin != -1) {
+    acceptlogin = true;
+    inputLogin.classList.remove("wrong");
+  } else {
+    inputLogin.classList.add("wrong");
+    acceptlogin = false;
+  }
+
+  let inputPassword = document.getElementById("password");
+  let password = document.getElementById("password").value;
+  let verifpassword = password.split("");
+  let number = 0;
+  let acceptpassword = false;
+
+  for (let i = 0; i <= verifpassword.length; i++) {
+    if (isNaN(verifpassword[i])) {
+      continue;
+    } else {
+      number++;
+    }
+  }
+
+  if (number >= 2) {
+    acceptpassword = true;
+    inputPassword.classList.remove("wrong");
+  } else {
+    inputPassword.classList.add("wrong");
+    acceptpassword = false;
+  }
+
+  if (acceptlogin == true && acceptpassword == true) {
+    location.replace(
+      "file:///C:/Users/33621/Documents/competence%203a%20boostrap/index.html"
+    );
+  } else {
+    alert("L'email ou le mot de passe est incorrect !");
+  }
+}
+
+/*****************/
+
+(function ($) {
+  "use strict";
+
+  // manual carousel controls
+  $(".next").click(function () {
+    $(".carousel").carousel("next");
+    return false;
+  });
+  $(".prev").click(function () {
+    $(".carousel").carousel("prev");
+    return false;
+  });
+})(jQuery);
+
+/***************/
+
+$("#recent").click(function () {
+  $("#trierPar").html("<p>Les plus récents</p>");
+});
+
+$("#populaire").click(function () {
+  $("#trierPar").html("<p>Les plus populaires</p>");
+});
+
+$("#recomandation").click(function () {
+  $("#trierPar").html("<p>Vos recomandations</p>");
+});
+
