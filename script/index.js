8fc1375d2d7c9673001eb49e48340e693f25000b
quentinDacquet <82867473+quentinDacquet@users.noreply.github.com>
2021-06-16 17:50:09 +0200

    Delete script directory

--- script/index.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#btn_triangle');
-const com = document.querySelector('.bloc_comment');
-
-btn.addEventListener('click', () => {
-    com.classList.toggle('triangle')
-    btn.classList.toggle('rotate_triangle')
-})
